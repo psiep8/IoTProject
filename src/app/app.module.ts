aef46d1a832d264d5d6f02eb6728caf8741f4e68
alessandro.carestia <carestiaal@gmail.com>
2023-02-08 10:07:08 +0100

    dipendenze aggiunte (si spera)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatIconModule} from '@angular/material/icon';
 import { AppComponent } from './app.component';
+import { MyChartComponent } from './my-chart/my-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyChartComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/my-chart/my-chart.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-chart',
+  templateUrl: './my-chart.component.html',
+  styleUrls: ['./my-chart.component.css']
+})
+export class MyChartComponent {
+
+}
